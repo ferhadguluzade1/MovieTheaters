20be3614fec8609fa5cc74fe5b4adb07f04911fb
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-27 12:47:46 +0400

    Base.Class.cs

--- Base.Class.cs
+++ Base.Class.cs
@@ -1,5 +1,16 @@
- public abstract class BaseClass
- {
-     protected int Id;
-     protected string Name;
- }
+namespace HomeTaskFor27._10._25;
+
+public abstract class Base
+{
+    public int Id { get; protected set; }
+    public string Name { get; protected set; }
+    public static int idCounter = 0;
+    protected Base(string name)
+    {
+        Name = name;
+
+        
+    }
+
+
+}
